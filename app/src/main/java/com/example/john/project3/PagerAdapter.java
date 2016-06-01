16e2acba9319a1549a3816bcc1ba45320e1b5277
John <johnm589@yahoo.com>
2016-05-31 17:30:22 -0700

    tuesNight Premerge

--- app/src/main/java/com/example/john/project3/PagerAdapter.java
+++ app/src/main/java/com/example/john/project3/PagerAdapter.java
@@ -7,9 +7,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
-/**
- * Created by drewmahrt on 2/18/16.
- */
 public class PagerAdapter extends FragmentStatePagerAdapter {
     int mNumOfTabs;
 
