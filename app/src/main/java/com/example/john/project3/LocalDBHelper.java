645fa6415ad105cb81ba90df1bfcb25b5b7ac2f2
Darrell Griffin <DarrellG@Darrells-MacBook-Air.local>
2016-06-01 09:49:43 -0700

    adds local database files

--- app/src/main/java/com/example/john/project3/LocalDBHelper.java
+++ app/src/main/java/com/example/john/project3/LocalDBHelper.java
@@ -11,7 +11,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class LocalDBHelper extends SQLiteOpenHelper{
     private static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "RATING_DB.db";
-    public static final String RATING_TABLE_NAME = "RATING";
+    public static final String RATING_TABLE_NAME = "RATINGS";
     public static final String COL_ID = "_id";
     public static final String COL_RATING = "Rating";
     public static final String[] RATING_COLUMNS = {COL_ID, COL_RATING};
