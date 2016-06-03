087191929c6344db2cf6aa18a4ac78adedc287ad
Alexander Lombardo <alexanderjlombardo@gmail.com>
2016-06-03 12:07:29 -0700

    dummy commit

--- app/src/main/java/com/example/john/project3/StudentDetailsActivity.java
+++ app/src/main/java/com/example/john/project3/StudentDetailsActivity.java
@@ -1,14 +1,9 @@
 package com.example.john.project3;
 
 import android.content.ContentValues;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.RatingBar;
 
 public class StudentDetailsActivity extends AppCompatActivity {
     LocalDBHelper helper;
@@ -23,27 +18,27 @@ public class StudentDetailsActivity extends AppCompatActivity {
 //        setSupportActionBar(toolbar);
 
         helper = LocalDBHelper.getInstance(StudentDetailsActivity.this);
-        if (id >= 0) {
-            Cursor cursor = helper.getDescriptionById(id);
-            myRating = Float.parseFloat(cursor.getString(cursor.getColumnIndex(helper.COL_RATING)));
-            RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar1);
-
-            ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-                @Override
-                public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                    getRating(rating);
-                }
-            });
-
-            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-            fab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-                }
-            });
-        }
+//        if (id >= 0) {
+//            Cursor cursor = helper.getDescriptionById(id);
+//            myRating = Float.parseFloat(cursor.getString(cursor.getColumnIndex(helper.COL_RATING)));
+//            RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar1);
+//
+//            ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+//                @Override
+//                public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+//                    getRating(rating);
+//                }
+//            });
+//
+//            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//            fab.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                            .setAction("Action", null).show();
+//                }
+//            });
+//        }
     }
     private float getRating(float rating) {
         myRating = rating;
